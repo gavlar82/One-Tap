fb8d5de7edb91f5debe39cbe6b8ccdea154c19c2
gavlar82 <noreply>
2016-01-29 17:25:43 +0000

    Add component location.reel

---
+++ ui/location.reel/location.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/location.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Location
+ * @extends Component
+ */
+exports.Location = Component.specialize(/** @lends Location# */ {
+    constructor: {
+        value: function Location() {
+            this.super();
+        }
+    }
+});
